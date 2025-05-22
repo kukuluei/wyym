7e27cae45593e80d7539b58cbb0d6c00616f8d74
kukuluei <2890799413@qq.com>
2025-05-22 20:41:09 +0800

    message 17: change models for multiple users

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,8 @@
-from email.policy import default
-
 from django.db import models
 
-# Create your models here.
+class List(models.Model):
+    pass
+
 class Item(models.Model):
-    text=models.TextField(default='')
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.CASCADE,default=None)
