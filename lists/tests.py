ae70e2dbc2a5b6654d78d74d4aaa8dfc45cd8dba
kukuluei <2890799413@qq.com>
2025-05-22 18:47:00 +0800

    message 16: form URL of new list by POST

--- lists/tests.py
+++ lists/tests.py
@@ -42,9 +42,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 302)  # (2)
         self.assertEqual(response['location'], '/lists/the-new-page/')  # (3)
 
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
 class ListViewTest(TestCase):
 
@@ -57,8 +54,14 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')  # (1)
 
-class ListViewTest(TestCase):
+class NewListTest(TestCase):
 
-    def test_uses_list_template(self):
-        response = self.client.get('/lists/the-new-page/')
-        self.assertTemplateUsed(response, 'list.html')
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})  # (1)
+        self.assertRedirects(response, '/lists/the-new-page/')  # (2)
