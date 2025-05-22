9e3921452fe0bfd58f732f94316d6852807d9d1c
kukuluei <2890799413@qq.com>
2025-05-22 15:47:31 +0800

    message 11: Show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -25,12 +25,28 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
 
 class HomePageTest(TestCase):
-
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')  # 创建一个名为 'itemey 1' 的 Item 实例
+        Item.objects.create(text='itemey 2')  # 创建一个名为 'itemey 2' 的 Item 实例
+        response = self.client.get('/')  # 发送一个GET请求到根URL
+        self.assertIn('itemey 1', response.content.decode())  # 断言响应内容中包含 'itemey 1'
+        self.assertIn('itemey 2', response.content.decode())  # 断言响应内容中包含 'itemey 2'
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
